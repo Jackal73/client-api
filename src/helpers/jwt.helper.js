c7208827d2d4c10f2255c17e70f475409601c1f2
Shawn Kebel <fxrender@outlook.com>
2022-03-27 23:31:06 -0500

    :sparkles::card_file_box: store `refreshJWT` with DB `user data`

--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
+const { storeUserRefreshJWT } = require('../model/user/User.model');
 const { setJWT, getJwt } = require('./redis.helper');
 
 const createAccessJWT = async (email, _id) => {
   try {
-    const accessJWT = await jwt.sign(
-      { email },
+    const accessJWT = await jwt.sign({ email },
       process.env.JWT_ACCESS_SECRET, {
-        expiresIn: '15m',
+        expiresIn: "15m",
       });
 
     await setJWT(accessJWT, _id);
@@ -14,21 +14,22 @@ const createAccessJWT = async (email, _id) => {
     return Promise.resolve(accessJWT);
   } catch (error) {
     return Promise.reject(error);
-
   }
-
-
-
-
 };
 
-const createRefreshJWT = (payload) => {
-  const refreshJWT = jwt.sign(
-    { payload },
-    process.env.JWT_ACCESS_SECRET, {
-      expiresIn: '30d'});
+const createRefreshJWT = async (email, _id) => {
 
-  return Promise.resolve(refreshJWT);
+  try {
+    const refreshJWT = jwt.sign({ email },
+      process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "30d",
+      });
+
+    await storeUserRefreshJWT(_id, refreshJWT);
+    return Promise.resolve(refreshJWT);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
 
 module.exports = {
