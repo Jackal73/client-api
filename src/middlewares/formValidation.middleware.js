029b4ffe5fba8d692e150af4158f5100d0bf92b0
Shawn Kebel <fxrender@outlook.com>
2022-05-22 17:04:39 -0500

    :sparkles::lock: Add `serverside validation`  for create new ticket and client response

--- src/middlewares/formValidation.middleware.js
+++ src/middlewares/formValidation.middleware.js
@@ -13,6 +13,10 @@ const newPassword = Joi.string()
   .max(30)
   .required();
 
+const shortStr = Joi.string().min(2).max(50);
+const longStr = Joi.string().min(2).max(1000);
+const dt = Joi.date();
+
 const resetPassReqValidation = (req, res, next) => {
 
   const schema = Joi.object({email});
@@ -35,7 +39,42 @@ const updatePassValidation = (req, res, next) => {
   next();
 };
 
+const createNewTicketValidation = (req, res, next) => {
+
+    const schema = Joi.object({
+      subject: shortStr.required(),
+      sender: shortStr.required(),
+      message: longStr.required(),
+      issueDate: dt.required()
+    });
+
+    const value = schema.validate(req.body);
+
+    if (value.error) {
+      return res.json({status: "error", message: value.error.message});
+    }
+
+    next();
+};
+
+const replyTicketMessageValidation = (req, res, next) => {
+  const schema = Joi.object({
+    sender: shortStr.required(),
+    message: longStr.required()
+  });
+
+  const value = schema.validate(req.body);
+
+  if (value.error) {
+    return res.json({status: "error", message: value.error.message});
+  }
+
+  next();
+};
+
 module.exports = {
   resetPassReqValidation,
   updatePassValidation,
+  createNewTicketValidation,
+  replyTicketMessageValidation,
 };
--- src/routers/ticket.router.js
+++ src/routers/ticket.router.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { insertTicket, getTickets, getTicketById, updateClientReply, updateStatusClose, deleteTicket } = require("../model/ticket/Ticket.model");
-const { userAuthorization } = require("../middlewares/authorization.middleware");
+const {
+	insertTicket,
+	getTickets,
+	getTicketById,
+	updateClientReply,
+	updateStatusClose,
+	deleteTicket
+} = require("../model/ticket/Ticket.model");
+const {
+	userAuthorization
+} = require("../middlewares/authorization.middleware");
+const {
+	createNewTicketValidation,
+	replyTicketMessageValidation
+} = require("../middlewares/formValidation.middleware");
 
 router.all("/", (req, res, next) => {
 
@@ -9,40 +22,44 @@ router.all("/", (req, res, next) => {
 });
 
 // - create new ticket
-router.post('/', userAuthorization, async (req, res) => {
-	try {
-		const{ subject, sender, message } = req.body;
-		const userId = req.userId;
+router.post(
+	'/',
+	createNewTicketValidation,
+	userAuthorization,
+	async (req, res) => {
+		try {
+			const{ subject, sender, message } = req.body;
+			const userId = req.userId;
 
-		const ticketObj = {
-			clientId: userId,
-			subject,
-			conversations: [
-				{
-					sender,
-					message,
-				},
-			],
-		};
+			const ticketObj = {
+				clientId: userId,
+				subject,
+				conversations: [
+					{
+						sender,
+						message,
+					},
+				],
+			};
 
-		const result = await insertTicket(ticketObj);
+			const result = await insertTicket(ticketObj);
 
-		if (result._id) {
-			return res.json({
-				status: "success",
-				message: "New ticket has been created",
+			if (result._id) {
+				return res.json({
+					status: "success",
+					message: "New ticket has been created",
+				});
+			}
+			res.json({
+				status: "error",
+				message: "Unable to create a ticket, please try again later",
+			});
+		} catch (error) {
+			res.json({
+				status: "error",
+				message: error.message
 			});
 		}
-		res.json({
-			status: "error",
-			message: "Unable to create a ticket, please try again later",
-		});
-	} catch (error) {
-		res.json({
-			status: "error",
-			message: error.message
-		});
-	}
 });
 
 // - get all tickets for a specific user
@@ -88,7 +105,7 @@ router.get('/:_id', userAuthorization, async (req, res) => {
 });
 
 // - update reply message from client
-router.put('/:_id', userAuthorization, async (req, res) => {
+router.put('/:_id', replyTicketMessageValidation, userAuthorization, async (req, res) => {
 
 	try {
 		const {message, sender} = req.body;
